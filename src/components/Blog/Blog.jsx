078b01f6f0032671922f26510e1b2d084ec6c33e
hasnat-shahriyar <hasnatshahriyarnahin1@gmail.com>
2023-12-18 15:44:02 +0600

    blog card css addition

--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,34 +1,42 @@
 import { faBookmark } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-const Blog = ({blog}) => {
+import './Blog.css';
+const Blog = ({blog, AddToBookmark, MarkAsRead}) => {
     const {id, title, cover, author, author_img, posted_date, reading_time, hashtags} = blog;
+    
     return (
-        <div>
-            <img src={cover} alt={`Title Image ${title}`} />
-
-            <div>
-                <img src={author_img} alt="" />
-                <div>
-                    <h3>{author}</h3>
-                    <p>{posted_date}</p>
+        <div className='blog-container'>
+            <div className='blog-banner'>
+                <img src={cover} alt={`Title Image ${title}`} />
+            </div>
+            <div className='blog-info'>
+                <div className='blog-author'>
+                    <img src={author_img} alt="" />
+                    <div className='info'>
+                        <h3>{author}</h3>
+                        <p>{posted_date}</p>
+                    </div>
+                </div>
+                <div className='bookmark'>
+                    <p className='read-time'>{reading_time} min read</p>
+                    <button 
+                    onClick={() => AddToBookmark(blog)}
+                    className='btn-bookmark'
+                    >
+                        <FontAwesomeIcon icon={faBookmark} />
+                    </button>
                 </div>
             </div>
-            <div>
-                <span>{reading_time} min read</span>
-                <button 
-                onClick={() => handleAddToBookmark(blog)}
-                className='btn-bookmark'
-                >
-                    <FontAwesomeIcon icon={faBookmark} />
-                </button>
-            </div>
-            <h2>{title}</h2>
+            <h2 className='title'>{title}</h2>
             <p>
                 {
                     hashtags.map((hash, idx) => <span key = {idx}> <a href=''>#{hash}</a> </span>)
                 }
             </p>
+            <button onClick={() => MarkAsRead(id, reading_time)} className='btn-mark'>
+                Mark As Read
+            </button>
         </div>
     );
 };
